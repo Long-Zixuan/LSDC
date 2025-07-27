bf904bc792ca55657f03274cdd3bd400c30c38a9
Long-Zixuan <778275146@qq.com>
2025-07-27 19:35:59 +0800

    更改了SystemAndGLInfo的初始化逻辑 LZX-2025-07-27-009

--- src/main/java/me/loongly/mods/lsdc/EmbeddiumDeviceCheckMod.java
+++ src/main/java/me/loongly/mods/lsdc/EmbeddiumDeviceCheckMod.java
@@ -1,11 +1,14 @@
 package me.loongly.mods.lsdc;
 
+import me.loongly.mods.lsdc.api.system.SystemAndGLInfo;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(EmbeddiumDeviceCheckMod.MOD_ID)
 public final class EmbeddiumDeviceCheckMod {
     public static final String MOD_ID = "embeddium_device_check";
 
-    public EmbeddiumDeviceCheckMod() {
+    public EmbeddiumDeviceCheckMod()
+    {
+        SystemAndGLInfo.initInstance();
     }
 }
